a7d714045665dd8d5767fb245335c15bb4015b64
Helen Koike <helen@koikeco.de>
2022-03-27 18:48:44 -0300

    Start main page structure - lineup

---
+++ src/components/MrLineup.vue
@@ -0,0 +1,56 @@
+<template>
+    <ion-card>
+        <ion-slides>
+            <ion-slide>
+                <p>Texto teste 1</p>
+            </ion-slide>
+            <ion-slide>
+                <p>Texto teste 2</p>
+            </ion-slide>
+            <ion-slide>
+                <p>Texto teste 3</p>
+            </ion-slide>
+        </ion-slides>
+    </ion-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonCard, IonSlides, IonSlide } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'MrLineup',
+  components: {
+    IonCard,
+    IonSlides,
+    IonSlide,
+  }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
--- src/views/MrPageMain.vue
+++ src/views/MrPageMain.vue
@@ -3,10 +3,12 @@
     <MrHeader />
 
     <ion-content :fullscreen="true">
-      <div id="container">
-        <strong class="capitalize">Mundo Recicladores</strong>
-        <p>Vamos fazer um app</p>
-      </div>
+      <MrLineup />
+      <MrComoChegar />
+      <MrAds />
+      <MrNivel />
+      <MrRanking />
+      <MrNoticias />
     </ion-content>
   </ion-page>
 </template>
@@ -15,6 +17,7 @@
 import { defineComponent } from 'vue';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import MrHeader from '../components/MrHeader.vue'
+import MrLineup from '../components/MrLineup.vue'
 
 export default defineComponent({
   name: 'MrPageMain',
@@ -22,33 +25,7 @@ export default defineComponent({
     IonContent,
     IonPage,
     MrHeader,
+    MrLineup,
   }
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
