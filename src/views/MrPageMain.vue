a30cd7bc370fe8ca141f6346d0350665338aab9e
Helen Koike <helen@koikeco.de>
2022-03-28 01:01:29 -0300

    MrPageMain: grid components

--- src/views/MrPageMain.vue
+++ src/views/MrPageMain.vue
@@ -1,21 +1,48 @@
 <template>
   <ion-page>
+    <div id="container">
     <MrHeader />
 
     <ion-content :fullscreen="true">
-      <MrLineup />
-      <MrComoChegar />
-      <MrAds />
-      <MrNivel />
-      <MrRank />
-      <MrNoticias />
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <MrLineup />
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <MrComoChegar />
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <MrAds />
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <MrNivel />
+            </ion-col>
+            <ion-col>
+              <MrRank/>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <MrNoticias />
+            </ion-col>
+          </ion-row>
+        </ion-grid>
     </ion-content>
+    </div>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonGrid, IonCol, IonRow } from '@ionic/vue';
 import MrHeader from '../components/MrHeader.vue'
 import MrLineup from '../components/MrLineup.vue'
 import MrComoChegar from '../components/MrComoChegar.vue'
@@ -29,6 +56,9 @@ export default defineComponent({
   components: {
     IonContent,
     IonPage,
+    IonGrid,
+    IonCol,
+    IonRow,
     MrHeader,
     MrLineup,
     MrComoChegar,
@@ -39,3 +69,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+</style>
