b5e0db21bcd527f16fbf91a91995ab61b6900155
Helen Koike <helen@koikeco.de>
2022-04-06 15:38:22 -0300

    MrHeader: base

--- src/components/MrHeader.vue
+++ src/components/MrHeader.vue
@@ -1,11 +1,18 @@
 <template>
     <ion-header :translucent="true">
       <ion-toolbar>
+
         <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
+          <ion-menu-button></ion-menu-button>
         </ion-buttons>
-        <ion-title>Mundo Recicladores</ion-title>
+
+        <ion-title size=small><b>Nível 0 - Recicla pontos</b></ion-title>
+
+        <ion-buttons slot="end">
+          <ion-icon slot="icon-only" :icon="personCircle"></ion-icon>
+        </ion-buttons>
       </ion-toolbar>
+
     </ion-header>
     
     <ion-content :fullscreen="true">
@@ -19,7 +26,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/vue';
+import { personCircle } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'MrHeader',
@@ -29,7 +37,23 @@ export default defineComponent({
     IonHeader,
     IonMenuButton,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonIcon,
+  },
+  setup() {
+    return {
+      personCircle,
+    };
+  },
 });
 </script>
+
+<style scoped>
+ion-toolbar {
+  --background: var(--ion-color-primary);
+  --color: var(--ion-color-primary-contrast);
+}
+ion-menu-button {
+  --color: var(--ion-color-primary-contrast)
+}
+</style>
