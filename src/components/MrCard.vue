a278d48acc991582be03f16ea8a4819fef565927
Helen Koike <helen@koikeco.de>
2022-05-18 15:13:12 -0300

    MrCard: add ion-card-content

--- src/components/MrCard.vue
+++ src/components/MrCard.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="container">
         <ion-card>
+            <ion-card-content>
             <slot></slot>
+            </ion-card-content>
         </ion-card>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonCard } from '@ionic/vue';
+import { IonCard, IonCardContent } from '@ionic/vue';
 
 export default defineComponent({
   name: 'MrCard',
   components: {
     IonCard,
+    IonCardContent,
   },
 });
 </script>
@@ -31,5 +34,9 @@ ion-card {
   background-color: var(--ion-color-primary-contrast);
   border-radius: 8px;
   width: 100%;
+}
+ion-card-content {
+  margin: 0;
+  padding: 0;
 }
 </style>
