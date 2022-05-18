051e1fa0c38d32f4f439635b789f74d31fae2a88
Helen Koike <helen@koikeco.de>
2022-05-18 14:42:56 -0300

    MrCard: use ion-card
    For some reason, using div is not helping with card headers in other
    components (contant doesn't stay contained in the card).
    
    Migrating to ion-card solved the issue.

--- src/components/MrCard.vue
+++ src/components/MrCard.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="container">
-      <div class="row">
-        <div class="card">
+        <ion-card>
             <slot></slot>
-        </div>
-      </div>
+        </ion-card>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonCard } from '@ionic/vue';
 
 export default defineComponent({
   name: 'MrCard',
+  components: {
+    IonCard,
+  },
 });
 </script>
 
@@ -23,20 +25,11 @@ export default defineComponent({
   display: flex;
   width: 100%;
 }
-.row {
+ion-card {
   align-items: center;
   justify-content: center;
-  display: flex;
-  width: 100%;
-  margin: 0 var(--mr-side-margin);
-}
-.card {
-  align-items: center;
-  justify-content: center;
-  display: flex;
   background-color: var(--ion-color-primary-contrast);
   border-radius: 8px;
   width: 100%;
-  box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
 }
 </style>
