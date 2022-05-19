ab6924fc999a6dae0c22d243dcb13376f61122c2
Helen Koike <helen@koikeco.de>
2022-05-19 15:38:19 -0300

    MrPerfil: add skeleton components

--- src/views/MrPerfil.vue
+++ src/views/MrPerfil.vue
@@ -1,11 +1,50 @@
 <template>
-    hello
+  <ion-page>
+    <div id="container">
+
+    <ion-content :fullscreen="true">
+    <div id="container_view">
+              <MrPerfilCard />
+              <MrMeuImpacto />
+              <MrColeta />
+              <MrBeneficios />
+              <MrSair/>
+    </div>
+    </ion-content>
+    </div>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IonContent, IonPage, } from '@ionic/vue';
 
 export default defineComponent({
-  name: 'MrPerfil',
+  name: 'MrPageMain',
+  components: {
+    IonContent,
+    IonPage,
+  }
 });
 </script>
+
+<style scoped>
+ion-content {
+  --padding-bottom: 0;
+}
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+#container_view {
+  max-width: 700px;
+  margin: auto;
+}
+.separator {
+  width: 100%;
+  height: 20px;
+}
+</style>
