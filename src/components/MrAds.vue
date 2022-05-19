ed145407a9b38faff720a9b2f204b4fd0dff7259
Helen Koike <helen@koikeco.de>
2022-05-19 14:57:14 -0300

    MrAds: use MrCard
    It is working now, I believe this is because of ion-card in MrCard

--- src/components/MrAds.vue
+++ src/components/MrAds.vue
@@ -1,37 +1,24 @@
 <template>
-
-  <table>
-  <tr>
-    <th class=table_margin>
-    </th>
-
-    <th>
-        <ion-card>
-          <swiper
-            :autoplay="{delay:4500, disableOnInteraction: false}"
-            :modules="modules"
-            >
-            <swiper-slide v-for="ads in slowdata.ads" :key="ads.id">
-              <div class="keep_aspect_ratio">
-                <ion-img :src="ads"></ion-img>
-              </div>
-            </swiper-slide>
-          </swiper>
-        </ion-card>
-    </th>
-
-    <th class=table_margin>
-    </th>
-  </tr>
-  </table>
-
+  <MrCard>
+    <swiper
+      :autoplay="{delay:4500, disableOnInteraction: false}"
+      :modules="modules"
+      >
+      <swiper-slide v-for="ads in slowdata.ads" :key="ads.id">
+        <div class="keep_aspect_ratio">
+          <ion-img :src="ads"></ion-img>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </MrCard>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
-import { IonCard, IonImg } from '@ionic/vue';
+import { IonImg } from '@ionic/vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Autoplay } from "swiper";
+import MrCard from '@/components/MrCard.vue';
 
 // Import Swiper styles
 import "swiper/css";
@@ -42,10 +29,10 @@ import "swiper/css/autoplay";
 export default defineComponent({
   name: 'MrAds',
   components: {
-    IonCard,
     IonImg,
     Swiper,
     SwiperSlide,
+    MrCard,
   },
   inject: ['slowdata'],
   setup() {
@@ -57,40 +44,11 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.table_margin {
-  width: var(--mr-side-margin);
-  margin: 0 auto;
-}
-ion-card {
-  border-radius: 8px;
-  margin: 0;
-}
 .keep_aspect_ratio {
   padding: 0;
   margin: 0;
   width: 100%;
   height: 0;
   padding-bottom: 25%;
-}
-ion-button {
-  margin: auto;
-  position: relative;
-  left: -6px;
-}
-p {
-  font-size: 15px;
-  line-height: 25px;
-  text-align: left;
-}
-table {
-  width: 100%;
-  margin: 0;
-  table-layout:fixed;
-}
-th {
-  margin: 0;
-}
-tr {
-  margin: 0;
 }
 </style>
