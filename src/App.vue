590fcfb9505208eb748d498bbe9b41abf0eb8a2a
Helen Koike <helen@koikeco.de>
2022-06-03 20:21:20 -0300

    App.vue: add default programacao

--- src/App.vue
+++ src/App.vue
@@ -97,8 +97,525 @@ const default_app_data = {
     ],
   },
   programacao: {
-    ver: 0,
-    content: [],
+  "ver": 559,
+  "content": [
+    {
+      "inicio": "2022-06-16T13:00:00.000Z",
+      "duracao_min": 600,
+      "nome": "Soundsystem | Jurassic | Paz e Dub",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-16T17:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Prezado 5.0",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-16T18:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Mil Pássaros Dançando",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-16T19:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Bloco da Bilinha",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-16T20:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Karola Nunes",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-16T21:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Capa e seus Grilos",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-16T21:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Catarina no Sofá",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-16T22:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Liniker",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-16T22:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Zaion",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-16T22:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Trio da Gala",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-16T23:30:00.000Z",
+      "duracao_min": 1230,
+      "nome": "Cabelo",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-17T00:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Marina Dagher e Klebin Vita",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-17T00:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Luahu",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T01:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "O Grande Encontro",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-17T02:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Luísa e os Alquimistas",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-17T02:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Sálvia da Terra",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-17T03:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Diana do Sertão",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T04:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Falamansa",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-17T04:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Irmãos Maccoy",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-17T05:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Forró Red Light",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-17T05:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Trio Praiano",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T06:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "As Fulôs do Cerrado",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T07:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Nil Guimarães",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-17T07:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Forró di Muié",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T13:00:00.000Z",
+      "duracao_min": 600,
+      "nome": "Soundsystem | Jurassic | Paz e Dub",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-17T15:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Maria Orfina",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-17T15:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Empodera Elas",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-17T15:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Bombeiros do Sol",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-17T17:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Saravá",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-17T18:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Simba do Chicão",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-17T19:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Kauze",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-17T20:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Elisa de Sena",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-17T21:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Congadar",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-17T21:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Ric",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-17T21:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Felipe Sereno",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-17T22:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Tribo de Jah",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-17T22:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Netvno",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T00:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Bia Ferreira",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-18T00:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Xaxado Novo",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-18T00:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Umbrado",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-18T01:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Racionais",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-18T01:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Lança Xote",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T03:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Rapadura XC",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-18T03:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Quarteto Pé de Cabra",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-18T04:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Cordel do Fogo Encantado",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-18T04:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Rock in Roça",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T06:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Furmiga Dub e seu Bando",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-18T06:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Munduká",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-18T07:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Forró da Morada",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-18T13:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Batmacumba Samba Reggae",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T13:00:00.000Z",
+      "duracao_min": 600,
+      "nome": "Soundsystem | Jurassic | Paz e Dub",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-18T15:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Chic Hernandez",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-18T15:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Samba da Opinião",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T17:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Gnomos do Quintal",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-18T17:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Canavieira",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T17:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Berimbalata",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-18T19:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Bolero Freak",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-18T19:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Fundação Reggae",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-18T20:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Saravashivaya",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T21:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "El Efecto",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-18T21:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Duo Franz",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-18T23:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Mart'nália",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-18T23:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Maikão",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-18T23:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Valentina Guillen",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T00:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Mariana Aydar",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-19T00:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Fenícia",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-19T02:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Baiana System",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-19T02:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Cidrais",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-19T03:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Čao Laru",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-19T03:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Bafafá",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T04:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Francisco El Hombre",
+      "local": "Palco Vale"
+    },
+    {
+      "inicio": "2022-06-19T04:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Undercover",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-19T04:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Neide Garapá",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T05:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Pá de Manaca",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T06:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Ventania",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-19T07:00:00.000Z",
+      "duracao_min": 30,
+      "nome": "Premiação do Concurso Das Bandas De Forró",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T07:30:00.000Z",
+      "duracao_min": 90,
+      "nome": "Suzie Kill",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-19T07:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Show da Banda Campeã do Concurso de Forró",
+      "local": "Palco Forró"
+    },
+    {
+      "inicio": "2022-06-19T12:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Chorinho",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-19T13:00:00.000Z",
+      "duracao_min": 600,
+      "nome": "Soundsystem | Jurassic | Paz e Dub",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-19T14:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Aline Tavares",
+      "local": "Palco Dharma"
+    },
+    {
+      "inicio": "2022-06-19T15:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Samba de Quinta com Rafaela Vaz",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-19T15:00:00.000Z",
+      "duracao_min": 60,
+      "nome": "Enraíze",
+      "local": "Palco Piscina"
+    },
+    {
+      "inicio": "2022-06-19T16:00:00.000Z",
+      "duracao_min": 90,
+      "nome": "Santa Jam Vó Alberta",
+      "local": "Palco Mirante"
+    },
+    {
+      "inicio": "2022-06-19T17:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Perê Piano",
+      "local": "Palco Praça do Lago"
+    },
+    {
+      "inicio": "2022-06-19T17:30:00.000Z",
+      "duracao_min": 60,
+      "nome": "Relô-Rolô",
+      "local": "Palco Piscina"
+    }
+  ]
   },
   coletas: {
     ver: 0,
